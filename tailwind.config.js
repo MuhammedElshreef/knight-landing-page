b8619289f20ec71d49c10223e72e6f7202e78790
Muhammed Al-sayed <elshreef.game@gmail.com>
2023-11-03 22:26:07 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        "play-pen": ["Playpen Sans", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
